a4d70f1e5d4086ed77c355a044ad5c72cb440b60
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-08 08:15:00 +0000

    solution, finished

--- main.py
+++ main.py
@@ -26,4 +26,39 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random
 
+player_choice = int(input("What do you choose? 0 for Rock, 1 for Paper, 2 for Scissors.\n "))
+
+computer_choice = random.randint(0, 2)
+
+# Outputs drawing of rock, paper, and scissors for player and computer
+print(player_choice)
+if player_choice == 0:
+  print(rock)
+elif player_choice == 1:
+  print(paper)
+elif player_choice == 2:
+  print(scissors)
+ 
+print(computer_choice)
+if computer_choice == 0:
+  print(rock)
+elif computer_choice == 1:
+  print(paper)
+elif computer_choice == 2:
+  print(scissors)
+
+# Determines Winner Logic
+if player_choice >= 3 or player_choice < 0:
+  print("Invalid selection you lose.")
+elif player_choice == 0 and computer_choice == 2:
+  print("You Win!")
+elif computer_choice == 0 and player_choice == 2:
+  print("You lose.")
+elif player_choice > computer_choice:
+  print("You Win!")
+elif computer_choice > player_choice:
+  print("You lose.")
+elif computer_choice == player_choice:
+  print("Game results in a Draw.")
